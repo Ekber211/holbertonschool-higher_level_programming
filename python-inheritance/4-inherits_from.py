21c8a6882d70c523c6f48ecb3dc9b69639133585
Ekber211 <kbrquliyev621@gmail.com>
2026-03-20 15:05:44 +0400

    f

---
+++ python-inheritance/4-inherits_from.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Function that checks if an object inherits from a specified class."""
+
+
+def inherits_from(obj, a_class):
+    """Return True if obj is an instance of a subclass of a_class,
+    otherwise False.
+    """
+    return isinstance(obj, a_class) and type(obj) is not a_class
