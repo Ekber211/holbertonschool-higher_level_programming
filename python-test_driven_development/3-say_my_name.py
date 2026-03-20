e12360659c162297172c20d74f4b60cb6d8ee4f4
Ekber211 <kbrquliyev621@gmail.com>
2026-03-20 12:10:16 +0400

    f'

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -1,17 +1,12 @@
 #!/usr/bin/python3
-"""This module prints a name in a specific format."""
 
 
 def say_my_name(first_name, last_name=""):
-    """Prints the name.
+    """Prints 'My name is <first name> <last name>'."""
 
-    Raises TypeError if inputs are not strings.
-    """
-
-    if type(first_name) is not str:
+    if not isinstance(first_name, str):
         raise TypeError("first_name must be a string")
-
-    if type(last_name) is not str:
+    if not isinstance(last_name, str):
         raise TypeError("last_name must be a string")
 
     print("My name is {} {}".format(first_name, last_name))
